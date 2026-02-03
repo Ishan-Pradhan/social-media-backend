9dd220c67f6365c4a2958eadb1711b0a31a9c72b
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-03 10:07:06 +0545

    fix: change static localhost to req.protocol

--- src/controllers/apps/auth/user.controllers.js
+++ src/controllers/apps/auth/user.controllers.js
@@ -75,7 +75,9 @@ const registerUser = asyncHandler(async (req, res) => {
     subject: "Please verify your email",
     mailgenContent: emailVerificationMailgenContent(
       user.username,
-      `http://localhost:3000/users/verify-email/${unHashedToken}`
+      `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/verify-email/${unHashedToken}`
     ),
   });
 
@@ -254,7 +256,9 @@ const resendEmailVerification = asyncHandler(async (req, res) => {
     subject: "Please verify your email",
     mailgenContent: emailVerificationMailgenContent(
       user.username,
-      `http://localhost:3000/users/verify-email/${unHashedToken}`
+      `${req.protocol}://${req.get(
+        "host"
+      )}/api/v1/users/verify-email/${unHashedToken}`
     ),
   });
   return res
