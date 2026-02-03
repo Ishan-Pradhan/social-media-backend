1c689923f14756ea6c4a3cf2c3cbe4eecdf01848
Ishan Pradhan <ishanpradhan72@gmail.com>
2026-02-03 16:25:14 +0545

    feat: replace smtp with mailtrapper api

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,14 +1,12 @@
 import Mailgen from "mailgen";
-import nodemailer from "nodemailer";
 import { Product } from "../models/apps/ecommerce/product.models.js";
 import logger from "../logger/winston.logger.js";
+import axios from "axios";
 
 /**
- *
- * @param {{email: string; subject: string; mailgenContent: Mailgen.Content; }} options
+ * @param {{email: string; subject: string; mailgenContent: Mailgen.Content}} options
  */
 const sendEmail = async (options) => {
-  // Initialize mailgen instance with default theme and brand configuration
   const mailGenerator = new Mailgen({
     theme: "default",
     product: {
@@ -17,42 +15,37 @@ const sendEmail = async (options) => {
     },
   });
 
-  // For more info on how mailgen content work visit https://github.com/eladnava/mailgen#readme
-  // Generate the plaintext version of the e-mail (for clients that do not support HTML)
-  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
-
-  // Generate an HTML email with the provided contents
+  const emailText = mailGenerator.generatePlaintext(options.mailgenContent);
   const emailHtml = mailGenerator.generate(options.mailgenContent);
 
-  // Create a nodemailer transporter instance which is responsible to send a mail
-  const transporter = nodemailer.createTransport({
-    host: process.env.MAILTRAP_SMTP_HOST,
-    port: process.env.MAILTRAP_SMTP_PORT,
-    auth: {
-      user: process.env.MAILTRAP_SMTP_USER,
-      pass: process.env.MAILTRAP_SMTP_PASS,
+  const payload = {
+    from: {
+      email: process.env.MAILTRAP_FROM_EMAIL,
+      name: "FreeAPI",
     },
-  });
-
-  const mail = {
-    from: "mail.freeapi@gmail.com", // We can name this anything. The mail will go to your Mailtrap inbox
-    to: options.email, // receiver's mail
-    subject: options.subject, // mail subject
-    text: emailTextual, // mailgen content textual variant
-    html: emailHtml, // mailgen content html variant
+    to: [
+      {
+        email: options.email,
+      },
+    ],
+    subject: options.subject,
+    text: emailText,
+    html: emailHtml,
   };
 
   try {
-    console.log("Mail is  sent ");
-    await transporter.sendMail(mail);
+    await axios.post(`${process.env.MAILTRAP_API_URL}/api/send`, payload, {
+      headers: {
+        Authorization: `Bearer ${process.env.MAILTRAP_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Mail sent successfully via Mailtrap API");
   } catch (error) {
-    // As sending email is not strongly coupled to the business logic it is not worth to raise an error when email sending fails
-    // So it's better to fail silently rather than breaking the app
-    logger.error(
-      "Email service failed silently. Make sure you have provided your MAILTRAP credentials in the .env file"
-    );
-    console.log(error);
-    logger.error("Error: ", error);
+    logger.error("Mailtrap API email failed", {
+      error: error?.response?.data || error.message,
+    });
   }
 };
 
